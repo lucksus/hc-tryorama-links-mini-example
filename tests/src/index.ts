2829593e5273642d61faf82051acfebf61f0c0cc
Nicolas Luck <nicolas@lucksus.eu>
2021-09-15 00:38:02 +0200

    Import local files

---
+++ tests/src/index.ts
@@ -0,0 +1,50 @@
+import { Orchestrator, Config, InstallAgentsHapps, TransportConfigType } from "@holochain/tryorama";
+import path from "path";
+
+export const sleep = ms => new Promise(r => setTimeout(r, ms))
+
+const conductorConfig = Config.gen();
+const dm = path.join(__dirname, "../../workdir/links-test.dna");
+const installation: InstallAgentsHapps = [
+  // agent 0
+  [
+    // happ 0
+    [dm],
+  ],
+  // agent 1
+  //[
+    // happ 0
+  //  [dm],
+  //],
+];
+
+const ZOME = "links-test"
+
+const orchestrator = new Orchestrator();
+
+orchestrator.registerScenario("Show that links can be shared across agents", async (s, t) => {
+  const [alice, bob] = await s.players([conductorConfig, conductorConfig]);
+  //const [alice] = await s.players([conductorConfig]);
+  const [[alice_dm]] = await alice.installAgentsHapps(installation);
+  //const [[alice_dm], [bob_dm]] = await alice.installAgentsHapps(installation);
+  const [[bob_dm]] = await bob.installAgentsHapps(installation);
+
+  await sleep(2000)
+
+  await s.shareAllNodes([alice, bob])
+
+  // ----------------------------------------------
+  // ------------- Setup done ---------------------
+  // ----------------------------------------------
+
+  await alice_dm.cells[0].call(ZOME, "create", "test message");
+  const alices_entries = await alice_dm.cells[0].call(ZOME, "fetch_inbox");
+  t.equal(alices_entries.length, 1)
+
+  await sleep(10000)
+
+  const bobs_entries = await bob_dm.cells[0].call(ZOME, "fetch_inbox");
+  t.equal(bobs_entries.length, 1)
+});
+
+orchestrator.run();
---
+++ zomes/links-test/src/lib.rs
@@ -0,0 +1,44 @@
+use hdk::prelude::*;
+
+#[hdk_entry(id = "test_entry", visibility = "private")]
+#[derive(Clone)]
+pub struct TestEntry(String);
+
+impl Into<String> for TestEntry {
+    fn into(self) -> String {
+        self.0
+    }
+}
+
+entry_defs![
+    Path::entry_def(),
+    TestEntry::entry_def()
+];
+
+fn inbox_hash() -> ExternResult<EntryHash> {
+    let path = Path::from("inbox");
+    path.ensure()?;
+    Ok(path.hash()?)
+}
+
+#[hdk_extern]
+pub fn create(message: String) -> ExternResult<()> {
+    let entry = TestEntry(message);
+    let entry_hash = hash_entry(&entry)?;
+    create_entry(entry)?;
+    create_link(inbox_hash()?, entry_hash, LinkTag::new(String::from("message")))?;
+    debug!("Link created");
+    Ok(())
+}
+
+#[hdk_extern]
+pub fn fetch_inbox(_: ()) -> ExternResult<Vec<String>> {
+    let mut result = Vec::new();
+    for link in get_links(inbox_hash()?, Some(LinkTag::new(String::from("message"))))?.into_inner() {
+        debug!("Link found: {:?}", link);
+        let entry = get(link.target, GetOptions::latest())?.unwrap();
+        let test_entry = TestEntry::try_from(entry)?;
+        result.push(test_entry.into());
+    }
+    Ok(result)
+}
